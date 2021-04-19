4b9aa6101dd69822519aa6ab7ed26281e0fa9311
prince chauhan <bolbam13@gmail.com>
2021-04-19 11:22:35 +0530

    hash files

---
+++ hash.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct set{
+int value;
+int index;
+};
+struct set hashtable(int m){
+	struct set T[m];
+	return T[m];
+}
+
+void main(){
+}
+struct set *search(struct set T,int k){
+}
